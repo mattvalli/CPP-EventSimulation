d06304c80dd089c5b1a4cf7872c9078a8efe508d
Matthew Valli <matthew.valli@gmail.com>
2015-02-23 14:00:40 -0800

    Initial Commit

---
+++ EventSimulation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EventSimulation
+//
+//  Created by Matthew Valli on 23/02/2015.
+//  Copyright (c) 2015 Matthew Valli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
